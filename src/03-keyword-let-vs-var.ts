022aeba018247c35e23037f3312a52d434ea1ea2
VoN <sudo.von.contact@gmail.com>
2021-12-22 17:59:14 -0600

    feat: :sparkles: Add keyword let vs var

---
+++ src/03-keyword-let-vs-var.ts
@@ -0,0 +1,21 @@
+(function name() {
+    if(true){
+        let customer = "Mary";
+        console.log("[let] The name of the customer inside the block is " + customer);
+    }else{
+        /* 
+            Compile time error.
+            console.log("The name of the customer outside the block is " + customer);
+        */
+    }
+})();
+
+(function name() {
+    if(false){
+        /* Here comes the hoisting. */
+        var customer = "Mary";
+        console.log("[var] The name of the customer inside the block is " + customer);
+    }else{
+        console.log("[var] The name of the customer outside the block is " + customer);
+    }
+})();
